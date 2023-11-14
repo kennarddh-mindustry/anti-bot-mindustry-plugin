e96bcfc2bef85640f6bf53f4d2efed2ac0fd8ecf
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-11-14 10:34:38 +0700

    chore: Setup mindustry plugin

---
+++ src/kennarddh/AntiBot.java
@@ -0,0 +1,12 @@
+package kennarddh;
+
+import arc.util.Log;
+import mindustry.mod.Plugin;
+
+@SuppressWarnings("unused")
+public class AntiBot extends Plugin {
+    @Override
+    public void init() {
+        Log.info("[AntiBot] Loaded");
+    }
+}
