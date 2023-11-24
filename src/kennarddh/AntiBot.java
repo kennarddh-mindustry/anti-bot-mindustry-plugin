674ae916f87454d70f6d1f000cf13444cc74dd01
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-11-24 16:33:31 +0700

    feat: Kick without log

--- src/kennarddh/AntiBot.java
+++ src/kennarddh/AntiBot.java
@@ -4,13 +4,13 @@ import arc.Events;
 import arc.util.CommandHandler;
 import arc.util.Log;
 import mindustry.game.EventType;
+import mindustry.gen.KickCallPacket;
 import mindustry.mod.Plugin;
+import mindustry.net.NetConnection;
 
 @SuppressWarnings("unused")
 public class AntiBot extends Plugin {
     private final IPBlacklist ipBlacklist = new IPBlacklist();
-
-    private static final long oneYearMS = 1000L * 60 * 60 * 24 * 365;
 
     @Override
     public void init() {
@@ -18,7 +18,7 @@ public class AntiBot extends Plugin {
 
         Events.on(EventType.ConnectionEvent.class, con -> {
             if (ipBlacklist.contains(con.connection.address)) {
-                con.connection.kick("Your IP is detected as bot. If you can read this message I can assume you are not a bot.", oneYearMS);
+                kickConnectionWithoutLogging(con.connection, "Your IP is detected as bot. If you can read this message I can assume you are not a bot.");
             }
         });
     }
@@ -28,5 +28,17 @@ public class AntiBot extends Plugin {
         handler.register("is_bot_ip", "<ip>", "Check is bot ip", arg -> {
             Log.info(ipBlacklist.contains(arg[0]));
         });
+    }
+
+    /**
+     * Now you can Kick a connection with a reason but without logging it in the console. No more spam.
+     **/
+    private static void kickConnectionWithoutLogging(NetConnection connection, String reason) {
+        KickCallPacket packet = new KickCallPacket();
+
+        packet.reason = reason;
+
+        connection.send(packet, true);
+        connection.close();
     }
 }
